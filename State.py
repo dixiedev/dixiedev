fd8d694b8774f8b9424cbea69f76c99a6034e8d3
negroman <42720940+negroman@users.noreply.github.com>
2018-11-01 19:58:32 -0400

    py

---
+++ State.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# State
+
+
+# AL","GA","FL","MS","TN","KY
+
+states = ["AL","GA","FL","MS","TN","KY"]
+
+
+# make a dic  --> figure out what goes into
+
+
+"""
+State = 
+ref site
+b-name
+
+otherz
+"""
+
+#json dump
